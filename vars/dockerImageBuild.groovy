6d7ee51ba61b7a60dbc1116e77f8ce3cf2d1cd4c
hhk14 <harishahk14@gmail.com>
2023-07-10 19:11:41 +0530

    added docker image build groovy script

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,5 +1,5 @@
 def call(String reponame, String imagename){
-   sh  'docker build -t ${reponame}/${imagename} .
-   sh 'docker tag "${reponame}/${imagename}  ${reponame}/${imagename}:${imagetag}"'
-   sh 'docker tag "${reponame}/${imagename}  ${reponame}/${imagename}:latest'
+   sh  """docker build -t ${reponame}/${imagename} .
+          docker tag ${reponame}/${imagename}  ${reponame}/${imagename}:${imagetag}
+          docker tag ${reponame}/${imagename}  ${reponame}/${imagename}:latest """
 }
