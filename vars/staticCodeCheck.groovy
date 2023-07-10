1aeae33cab26cf5a5764c665a3fe70dcaa669d61
hhk14 <harishahk14@gmail.com>
2023-07-10 16:59:33 +0530

    add static code check method to  shared library

--- vars/staticCodeCheck.groovy
+++ vars/staticCodeCheck.groovy
@@ -1,5 +1,5 @@
 def call(String sonarToken){
-    withCredentials([string(credentialsId: "${sonarToken}", variable: 'sonarIntegration')]) {
-    sh 'mvn  clean package sonar:sonar mvn -Dsonar.token="${sonarIntegration}"'
-}
+     
+    sh 'mvn  clean package sonar:sonar mvn -Dsonar.token="${sonarToken}"'
+
 }
