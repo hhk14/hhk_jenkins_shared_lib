d4c57e116945261fff18bf87adea898313bc5263
hhk14 <harishahk14@gmail.com>
2023-07-12 12:01:42 +0530

    added pitest groovy method

--- vars/piTest.groovy
+++ vars/piTest.groovy
@@ -1,3 +1,8 @@
 def call(){
     sh " mvn org.pitest:pitest-maven:mutationCoverage"
+    post{
+        always{
+            pitmutation mutationStatsFile: '**/target/pit-reports/**/mutations.xml'
+        }
+    }
 }
